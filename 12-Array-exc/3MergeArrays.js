14b470ea4781f119701abaa0e9bf587e1abdae93
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-06-07 02:44:06 +0300

    Add files via upload

---
+++ 12-Array-exc/3MergeArrays.js
@@ -0,0 +1,19 @@
+// Write a function, which receives two string arrays and merges them into a third array.  
+// •	If the index of the element is even, add into the third array the sum of both elements at that index
+// •	If the index of the element is odd, add the concatenation of both elements at that index
+// Input
+// As input, you will receive two string arrays (with equal length).
+// Output
+// As output, you should print the resulting third array, each element separated by " - ".
+function merge(firstArray, secondArray){
+    let thirdArray = [];
+    for(i=0; i<firstArray.length; i++){
+        if (i % 2 == 0){
+            thirdArray.push(Number(firstArray[i]) + Number(secondArray[i]));
+        }else {
+             thirdArray.push(firstArray[i].toString() + secondArray[i].toString());
+            }
+    }console.log(thirdArray.join(` - `))
+}
+merge(['5', '15', '23', '56', '35'],
+['17', '22', '87', '36', '11'])
