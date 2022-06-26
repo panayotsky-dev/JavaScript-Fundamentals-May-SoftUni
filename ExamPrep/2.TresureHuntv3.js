f7ff5810b5603af4db35c2df71e497cd7dc2f6c0
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-06-26 04:54:06 +0300

    Add files via upload

---
+++ ExamPrep/2.TresureHuntv3.js
@@ -0,0 +1,42 @@
+//*version 3 // 80/100
+function tresureHunt(data){
+let state = data.shift().split(`|`);
+let index =0;
+let command = data[index];
+index++;
+while(command != `Yohoho!`){
+    let tokens = command.split(` `);
+    let operation = tokens.shift();
+    switch(operation){
+        case `Loot`: for(let el of tokens){
+            if(!state.includes(el)){
+                state.unshift(el)
+            }
+        }
+             break;
+        case `Drop`: 
+         let i = Number(tokens[0]);
+        let element = state.splice(i, 1);
+        state.push(element[0]);
+        break;
+        case `Steal`: 
+        let count = Number(tokens[0]);
+        let items = state.splice(-count);
+        console.log(items.join(', '));
+        break;
+    }
+    command = data[index]
+    index++;
+}
+
+    let sum = 0;
+    for(let el of state){
+        sum+=el.length;
+    }
+    if (state.length > 0){
+    let avg = sum/state.length;
+    console.log(`Average treasure gain: ${avg.toFixed(2)} pirate credits.`)
+}else {console.log(`Failed treasure hunt.`)}
+
+}
+tresureHunt(["Gold|Silver|Bronze|Medallion|Cup","Loot Wood Gold Coins","Loot Silver Pistol","Drop 3","Steal 3","Yohoho!"])
