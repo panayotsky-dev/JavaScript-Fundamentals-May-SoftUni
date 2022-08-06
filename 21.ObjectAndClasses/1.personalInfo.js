202075437858c20fec06112517a6a5f8a57abea8
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-08-06 13:18:52 +0300

    Add files via upload

---
+++ 21.ObjectAndClasses/1.personalInfo.js
@@ -0,0 +1,11 @@
+function personalInfo(firstName,lastName,age){
+let myInfo = {};
+myInfo.firstName = firstName;
+myInfo.lastName = lastName;
+myInfo.age = age;
+return myInfo;
+}
+personalInfo("Peter", 
+"Pan",
+"20"
+)
